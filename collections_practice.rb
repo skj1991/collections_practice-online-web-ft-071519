eae2f5d67931f98a3a2e644ec38b53a702d6d999
Stacy Johns <stacykjohns@icloud.com>
2019-07-23 01:50:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,12 @@ def sort_array_desc(array)
   array.sort.reverse
 end
 
+def sort_array_char_count(array)
+  array.sort_by do |word|
+    word.length
+  end
+end
+
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
